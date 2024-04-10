e1626087239193850330556b3672ac42c09576a5
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-04-10 09:35:04 +0200

    Added Project File

---
+++ TaskTwo_tk/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TaskTwo_tk"
+include(":app")
+ 
