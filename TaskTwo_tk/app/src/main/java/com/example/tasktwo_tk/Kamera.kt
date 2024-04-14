907a60174f043ab50e0460c45b0809a1887bcbf3
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-04-15 01:30:58 +0200

    Added Design For Camera Activity

---
+++ TaskTwo_tk/app/src/main/java/com/example/tasktwo_tk/Kamera.kt
@@ -0,0 +1,11 @@
+package com.example.tasktwo_tk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Kamera : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kamera)
+    }
+}
