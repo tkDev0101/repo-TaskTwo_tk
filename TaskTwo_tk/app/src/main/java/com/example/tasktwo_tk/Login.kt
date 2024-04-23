a663a0d643f55e34d35614fd78de71babb2d865b
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-04-23 21:51:34 +0200

    code in -> (:app)build.gradle.kts
        buildFeatures {
            viewBinding = true
        }

--- TaskTwo_tk/app/src/main/java/com/example/tasktwo_tk/Login.kt
+++ TaskTwo_tk/app/src/main/java/com/example/tasktwo_tk/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.tasktwo_tk.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
